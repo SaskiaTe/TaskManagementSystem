dd70ee866472b5daf8a83e702005389110eec588
SaskiaTe <saskia.tellenbach16@gmail.ch>
2024-12-13 11:18:35 +0100

    Tasklist vollständig programmiert

---
+++ TaskManagementSystem/Tasklist.cs
@@ -0,0 +1,40 @@
+namespace TaskManagementSystem;
+
+public class Tasklist
+{
+    private List<Task> _tasks;
+
+    public void addTask(Task toBeAddedTask)
+    {
+        _tasks.Add(toBeAddedTask);
+    }
+
+    public void removeTask(Task toBeRemovedTask)
+    {
+        _tasks.Remove(toBeRemovedTask);
+    }
+
+    public List<Task> getAllTasks()
+    {
+        return _tasks;
+    }
+
+    public List<Task> filterTasks(Status statusToFilter)
+    {
+        List<Task> filteredTasks = new List<Task>();
+        for (int i = 0; i < _tasks.Count; i++)
+        {
+            if (statusToFilter == _tasks.ElementAt(i).Status)
+            {
+                filteredTasks.Add(_tasks.ElementAt(i));
+            }
+        }
+        return filteredTasks;
+    }
+
+    public void modifyTask(Task toBeModifiedTask, Task modifiedTask)
+    {
+        _tasks.Add(modifiedTask);
+        _tasks.Remove(toBeModifiedTask);
+    }
+}
