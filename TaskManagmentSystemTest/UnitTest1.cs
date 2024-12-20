27fc709c3a1728e7c5b6ad2c70b25d8bc31003da
LennyAmstutz <lennyamstutz@gmail.com>
2024-12-20 11:14:11 +0100

    Refacoring

--- TaskManagmentSystemTest/UnitTest1.cs
+++ TaskManagmentSystemTest/UnitTest1.cs
@@ -12,30 +12,24 @@ public class UserTest
         [TestMethod]
         public void TestGetName()
         {
-            // Arrange
             User user = new User(1, "John Doe");
 
-            // Act
             string name = user.getName();
 
-            // Assert
             Assert.AreEqual("John Doe", name);
         }
 
         [TestMethod]
         public void TestAssignTasks()
         {
-            // Arrange
             User user = new User(1, "John Doe");
             Task task1 = new Task("Task 1", "Description 1", DateOnly.FromDateTime(DateTime.Now), 1, 2, Status.OFFEN, user);
             Task task2 = new Task("Task 2", "Description 2", DateOnly.FromDateTime(DateTime.Now), 2, 3, Status.INBEARBEITUNG, user);
             List<Task> tasks = new List<Task> { task1, task2 };
 
-            // Act
             user.assignTasks(tasks);
             List<Task> assignedTasks = user.getAssignedTasks();
 
-            // Assert
             Assert.AreEqual(2, assignedTasks.Count);
             Assert.AreEqual(task1, assignedTasks[0]);
             Assert.AreEqual(task2, assignedTasks[1]);
@@ -44,16 +38,13 @@ public class UserTest
         [TestMethod]
         public void TestGetAssignedTasks()
         {
-            // Arrange
             User user = new User(1, "John Doe");
             Task task1 = new Task("Task 1", "Description 1", DateOnly.FromDateTime(DateTime.Now), 1, 2, Status.OFFEN, user);
             List<Task> tasks = new List<Task> { task1 };
 
-            // Act
             user.assignTasks(tasks);
             List<Task> assignedTasks = user.getAssignedTasks();
 
-            // Assert
             Assert.IsNotNull(assignedTasks);
             Assert.AreEqual(1, assignedTasks.Count);
             Assert.AreEqual(task1, assignedTasks[0]);
